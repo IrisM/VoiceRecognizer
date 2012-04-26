62a40a89897e6901fb4d29a4640499ca57e8eea8
Irina Brovar <245596436@rambler.ru>
2012-04-26 23:02:02 +0400

    Voice with variants

---
+++ src/com/example/android/VoiceRecognizer.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.example.android;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+
+public class VoiceRecognizer extends Activity implements OnClickListener {
+
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+    private ListView mList;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+        Button speakButton = (Button) findViewById(R.id.btn_speak);
+        mList = (ListView) findViewById(R.id.list);
+
+        // Check to see if a recognition activity is present
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(
+                new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        if (activities.size() != 0) {
+            speakButton.setOnClickListener(this);
+        } else {
+            speakButton.setEnabled(false);
+            speakButton.setText("Recognizer not present");
+        }
+    }
+
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_speak) {
+            startVoiceRecognitionActivity();
+        }
+    }
+
+
+    private void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);  
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ru-RU");
+
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+        	
+            // Fill the list view with the strings the recognizer thought it could have heard
+            ArrayList<String> matches = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);      
+            float[] confidence = data.getFloatArrayExtra(RecognizerIntent.EXTRA_CONFIDENCE_SCORES);
+            
+            try {
+				FileWriter vars = new FileWriter("variants.txt");
+				for (String match : matches){
+					vars.write(match);
+					vars.append('\t');
+				}
+				vars.append('\n');
+				vars.write(String.valueOf(confidence[0]));
+				vars.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+            
+            
+            int tmp = 0;
+            ArrayList<String> conf = new ArrayList<String>();
+            for (String ans : matches){
+            	conf.add(ans + "\n" + confidence[tmp]);
+            	tmp ++;
+            }
+            
+            mList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                    conf));
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
